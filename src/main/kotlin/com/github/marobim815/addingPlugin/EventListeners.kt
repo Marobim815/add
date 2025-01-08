32d0c3e3782d0e6edab3c68462b6500db81b7932
Marobim815 <be1wer0815@gmail.com>
2025-01-08 15:33:17 +0900

    기능 삭제

--- src/main/kotlin/com/github/marobim815/addingPlugin/EventListeners.kt
+++ src/main/kotlin/com/github/marobim815/addingPlugin/EventListeners.kt
@@ -6,9 +6,6 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPlaceEvent
 
 object EventListeners : Listener {
-
-    private const val VILLAGE_RANGE = 10 * 16
-
     @EventHandler
     fun onBlockPlace(event: BlockPlaceEvent) {
         // val player = event.player
